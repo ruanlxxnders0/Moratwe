699600483a6ce7b582f3f219182dbe70c9ba7433
“angxloooo” <angeloelhage906@gmail.com>
2025-08-12 12:22:12 +0200

    Login screen, don’t have an account should ask the user to register + made phone number a nullable field.

--- moratwe/urls.py
+++ moratwe/urls.py
@@ -153,6 +153,9 @@ urlpatterns = [
     # Events
     path('events/', include('events.urls')),
     
+    # Users
+    path('users/', include('users.urls')),
+    
     # Swagger documentation
     path('swagger<format>/', schema_view.without_ui(cache_timeout=0), name='schema-json'),
     path('swagger/', schema_view.with_ui('swagger', cache_timeout=0), name='schema-swagger-ui'),
